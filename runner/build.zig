601a5c7ef31173ae10b6bbc1bfdb7cce79c16c2c
Ritalin <kazu.ktz.tamura@gmail.com>
2024-06-25 15:24:13 +0900

    DCE

--- runner/build.zig
+++ runner/build.zig
@@ -122,47 +122,4 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
-
-
-///////////
-
-    const exe2 = b.addExecutable(.{
-        .name = "stage-extract-sql-placeholder",
-        .root_source_file = b.path("src/main2.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    exe2.linkSystemLibrary("zmq");
-    exe2.linkLibCpp();
-    exe2.linkLibC();
-    
-    exe2.root_module.addImport("zmq", dep_zzmq.module("zzmq"));
-    exe2.root_module.addImport("core", dep_core.module("core"));
-
-    b.installArtifact(exe2);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd2 = b.addRunArtifact(exe2);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd2.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd2.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step2 = b.step("run2", "Run the app");
-    run_step2.dependOn(&run_cmd2.step);
-
 }

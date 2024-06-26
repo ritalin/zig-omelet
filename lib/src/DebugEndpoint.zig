d81512399a772ab1a43fc67c2c45c575d781e7b3
Ritalin <kazu.ktz.tamura@gmail.com>
2024-06-26 12:14:38 +0900

    CLI引数処理の準備

---
+++ lib/src/DebugEndpoint.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+const types = @import("./types.zig");
+
+// /// (control) Server -> Client
+const CMD_S2C_BIND_PORT = std.fmt.comptimePrint("ipc://{s}/cmd_s2c", .{types.CHANNEL_ROOT});
+const CMD_S2C_CONN_PORT = std.fmt.comptimePrint("ipc://{s}/cmd_s2c", .{types.CHANNEL_ROOT});
+// /// (source) Client -> Server
+const REQ_C2S_BIND_PORT = std.fmt.comptimePrint("ipc://{s}/req_c2s", .{types.CHANNEL_ROOT});
+const REQ_C2S_CONN_PORT = std.fmt.comptimePrint("ipc://{s}/req_c2s", .{types.CHANNEL_ROOT});
+
+const StageChannel = std.StaticStringMap(types.Symbol).initComptime(.{
+    .{"--request-channel", std.fmt.comptimePrint("--request-channel={s}", .{REQ_C2S_CONN_PORT})},
+    .{"--subscribe-channel", std.fmt.comptimePrint("--subscribe-channel={s}", .{CMD_S2C_CONN_PORT})},
+});
+
+pub fn applyStageChannel(runner: *std.Build.Step.Run) !void {
+    for (StageChannel.keys()) |k| {
+        arg: {
+            for (runner.argv.items) |arg| {
+                if (std.meta.activeTag(arg) == .bytes) {
+                    if (std.mem.startsWith(u8, arg.bytes, k)) break :arg;
+                }
+            }
+            runner.addArg(StageChannel.get(k).?);
+        }
+    }
+}

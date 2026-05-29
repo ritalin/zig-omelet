60c0ce8d1dfd2302284492fd763f19165ab74dfd
Ritalin <kazu.ktz.tamura@gmail.com>
2026-05-29 16:19:42 +0900

    Add send log channel

--- lib/core/src/sockets/channels/SendChannel.zig
+++ lib/core/src/sockets/channels/SendChannel.zig
@@ -7,34 +7,69 @@ const events = root.events;
 const types = root.types;
 
 
-const Self = @This();
+const SendChannel = @This();
 
-pipe_id: u64,
-buffer: std.Io.Writer.Allocating,
-msg: nnng.Message,
-sender: nnng.PipeSender,
-stage: types.StageName,
+inner: InnerChannel,
 
-pub fn init(allocator: std.mem.Allocator, pipe_id: u64, stage_name: types.StageName, sender: nnng.PipeSender) !Self {
+pub fn init(allocator: std.mem.Allocator, pipe_id: u64, stage_name: types.StageName, sender: nnng.PipeSender) !SendChannel {
     return .{
-        .pipe_id = pipe_id,
-        .buffer = std.Io.Writer.Allocating.init(allocator),
-        .msg = try nnng.Message.create(),
-        .sender = sender,
-        .stage = stage_name,
+        .inner = try InnerChannel.init(allocator, pipe_id, stage_name, sender),
     };
 }
 
-pub fn deinit(self: *Self) void {
-    self.buffer.deinit();
+pub fn deinit(self: *SendChannel) void {
+    self.inner.deinit();
 }
 
-pub fn encode(self: *Self, event: events.Event) !void {
+pub fn encode(self: *SendChannel, event: events.Event) !void {
     std.log.scoped(.app).debug("SendQueue/event: {s}", .{@tagName(std.meta.activeTag(event))});
-
-    try encodeToCbor(&self.msg.writer, .{
-        .header = events.EventHeader.fromEvent(event) ,
-        .stage_name = self.stage,
-        .event = event,
-    });
+    return self.inner.encode(event);
 }
+
+const InnerChannel = struct {
+    pipe_id: u64,
+    buffer: std.Io.Writer.Allocating,
+    msg: nnng.Message,
+    sender: nnng.PipeSender,
+    stage: types.StageName,
+
+    pub fn init(allocator: std.mem.Allocator, pipe_id: u64, stage_name: types.StageName, sender: nnng.PipeSender) !InnerChannel {
+        return .{
+            .pipe_id = pipe_id,
+            .buffer = std.Io.Writer.Allocating.init(allocator),
+            .msg = try nnng.Message.create(),
+            .sender = sender,
+            .stage = stage_name,
+        };
+    }
+
+    pub fn deinit(self: *InnerChannel) void {
+        self.buffer.deinit();
+    }
+
+    pub fn encode(self: *InnerChannel, event: events.Event) !void {
+        try encodeToCbor(&self.msg.writer, .{
+            .header = events.EventHeader.fromEvent(event) ,
+            .stage_name = self.stage,
+            .event = event,
+        });
+    }
+};
+
+const Log = struct {
+    inner: InnerChannel,
+
+    pub fn init(allocator: std.mem.Allocator, pipe_id: u64, stage_name: types.StageName, sender: nnng.PipeSender) !SendChannel.Log {
+        return .{
+            .inner = try InnerChannel.init(allocator, pipe_id, stage_name, sender),
+        };
+    }
+
+    pub fn deinit(self: *SendChannel.Log) void {
+        self.inner.deinit();
+    }
+
+    pub fn encode(self: *SendChannel.Log, event: events.Event) !void {
+        return self.inner.encode(event);
+    }
+};

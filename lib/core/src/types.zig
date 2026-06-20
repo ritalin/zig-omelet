37877a1cd83b2db5edc6d00dc73d21de8e27cfc1
Ritalin <kazu.ktz.tamura@gmail.com>
2026-06-21 01:43:53 +0900

    move endpoint

--- lib/core/src/types.zig
+++ lib/core/src/types.zig
@@ -1,17 +1,5 @@
 const std = @import("std");
 
-pub const IPC_PROTOCOL = "ipc://";
-// IPC channel root directory
-pub const CHANNEL_ROOT = "/tmp/omelet";
-
-//
-// Channel endpoints
-//
-pub const REQ_PORT = "req_c2s";
-pub const PUBSUB_PORT = "cmd_s2c";
-pub const PUSHPULL_PORT = "push_c2s";
-pub const WORKER_ENDPOINT = "inproc://sync-thread";
-
 pub const Endpoints = struct {
     req_rep: Symbol,
     pub_sub: Symbol,
@@ -21,6 +9,7 @@ pub const Endpoints = struct {
 
 pub const FilePath = []const u8;
 pub const Symbol = []const u8;
+pub const SymbolZ = [:0]const u8;
 pub const BinaryData = []const u8;
 pub const StageName = []const u8;
 

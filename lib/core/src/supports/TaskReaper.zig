e4f30376f9d6d6dffa8078d746bca18d7af082b1
Ritalin <kazu.ktz.tamura@gmail.com>
2026-06-21 01:41:54 +0900

    Add generic task reaper

---
+++ lib/core/src/supports/TaskReaper.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+const BUFFER_SIZE = 4;
+
+buffer: [BUFFER_SIZE]ReapTask = undefined,
+select: std.Io.Select(ReapTask),
+
+const Self = @This();
+
+pub fn init(io: std.Io, allocator: std.mem.Allocator) !*Self {
+    const self = try allocator.create(Self);
+
+    self.* = .{
+        .select = .init(io, &self.buffer),
+    };
+
+    return self;
+}
+
+pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
+    self.select.cancelDiscard();
+    allocator.destroy(self);
+}
+
+pub fn detach(self: *Self, function: anytype, args: std.meta.ArgsTuple(@TypeOf(function))) std.Io.ConcurrentError!void {    
+    try self.select.concurrent(.processed, function, args);
+}
+
+pub fn tick(self: *Self) !void {
+    var results: [BUFFER_SIZE]ReapTask = undefined;
+    _ = try self.select.awaitMany(&results, 0);
+}
+
+const ReapTask = union(enum) {
+    processed: std.Io.Cancelable!void,
+};

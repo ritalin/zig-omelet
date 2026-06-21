f3f07bf4bc739bb0b5325e53f7420db3462daf50
Ritalin <kazu.ktz.tamura@gmail.com>
2026-06-21 15:57:01 +0900

    start remake

--- runner/src/settings/help.zig
+++ runner/src/settings/help.zig
@@ -4,85 +4,108 @@ const core = @import("core");
 
 const DescriptionItem = core.settings.DescriptionItem;
 
+// TODO:
 const ArgDescriptions = core.settings.DescriptionMap.initComptime(.{
-    // General
-    .{@tagName(.req_rep_channel), DescriptionItem{.desc = "Comminicate Req/Rep endpoint for zmq", .value = "CHANNEL",}},
-    .{@tagName(.pub_sub_channel), DescriptionItem{.desc = "Comminicate Pub/Sub endpoint for zmq", .value = "CHANNEL",}},
-    .{@tagName(.log_level), DescriptionItem{.desc = "Pass through log level (err / warn / info / debug / trace). default: info", .value = "LEVEL",}},
-    .{@tagName(.use_scope), DescriptionItem{.desc = "Use environment scope (default: default)", .value = "VALUE",}},
+//     // General
+//     .{@tagName(.req_rep_channel), DescriptionItem{.desc = "Comminicate Req/Rep endpoint for zmq", .value = "CHANNEL",}},
+//     .{@tagName(.pub_sub_channel), DescriptionItem{.desc = "Comminicate Pub/Sub endpoint for zmq", .value = "CHANNEL",}},
+//     .{@tagName(.log_level), DescriptionItem{.desc = "Pass through log level (err / warn / info / debug / trace). default: info", .value = "LEVEL",}},
+//     .{@tagName(.use_scope), DescriptionItem{.desc = "Use environment scope (default: default)", .value = "VALUE",}},
     .{@tagName(.help), DescriptionItem{.desc = "Print command-specific usage", .value = "",}},
-    // Commands
-    .{@tagName(.generate), DescriptionItem{.desc = "Generate query parameters/result-sets", .value = "",}},
-    .{@tagName(.@"init-default"), DescriptionItem{.desc = "Initialize subcommand default value environment", .value = "",}},
-    .{@tagName(.@"init-config"), DescriptionItem{.desc = "Initialize subcommand configuration environment", .value = "",}},
-    // Command/Generate
-    .{@tagName(.source_dir), DescriptionItem{.desc = "Source SQL folder(s) or file(s)", .value = "PATH", .required = true}},
-    .{@tagName(.output_dir), DescriptionItem{.desc = "Output folder", .value = "PATH", .required = true}},  
-    .{@tagName(.schema_dir), DescriptionItem{.desc = "Schema SQL folder", .value = "PATH", .required = true}},
-    .{@tagName(.include_filter), DescriptionItem{.desc = "Filter passing source/schema SQL directores or files satisfied (optional)", .value = "PATH"}},
-    .{@tagName(.exclude_filter), DescriptionItem{.desc = "Filter rejecting source/schema SQL directores or files satisfied (optional)", .value = "PATH"}},
-    // Command/init-default
-    .{@tagName(.subcommand), DescriptionItem{.desc = "Subcommand name", .value = "COMMAND", .required = true}},
-    .{@tagName(.new_scope), DescriptionItem{.desc = "init environment scope (default: default)", .value = "VALUE", .required = false}},
-    .{@tagName(.from_scope), DescriptionItem{.desc = "source environment scope (optional)", .value = "VALUE", .required = false}},
-    .{@tagName(.global), DescriptionItem{.desc = "Enable globally setting/config", .value = "", .required = false}},
+//     // Commands
+//     .{@tagName(.generate), DescriptionItem{.desc = "Generate query parameters/result-sets", .value = "",}},
+//     .{@tagName(.@"init-default"), DescriptionItem{.desc = "Initialize subcommand default value environment", .value = "",}},
+//     .{@tagName(.@"init-config"), DescriptionItem{.desc = "Initialize subcommand configuration environment", .value = "",}},
+//     // Command/Generate
+//     .{@tagName(.source_dir), DescriptionItem{.desc = "Source SQL folder(s) or file(s)", .value = "PATH", .required = true}},
+//     .{@tagName(.output_dir), DescriptionItem{.desc = "Output folder", .value = "PATH", .required = true}},  
+//     .{@tagName(.schema_dir), DescriptionItem{.desc = "Schema SQL folder", .value = "PATH", .required = true}},
+//     .{@tagName(.include_filter), DescriptionItem{.desc = "Filter passing source/schema SQL directores or files satisfied (optional)", .value = "PATH"}},
+//     .{@tagName(.exclude_filter), DescriptionItem{.desc = "Filter rejecting source/schema SQL directores or files satisfied (optional)", .value = "PATH"}},
+//     // Command/init-default
+//     .{@tagName(.subcommand), DescriptionItem{.desc = "Subcommand name", .value = "COMMAND", .required = true}},
+//     .{@tagName(.new_scope), DescriptionItem{.desc = "init environment scope (default: default)", .value = "VALUE", .required = false}},
+//     .{@tagName(.from_scope), DescriptionItem{.desc = "source environment scope (optional)", .value = "VALUE", .required = false}},
+//     .{@tagName(.global), DescriptionItem{.desc = "Enable globally setting/config", .value = "", .required = false}},
 });
 
 const GeneralSetting = @import("./commands/GeneralSetting.zig");
 pub const GeneralSettingArgId = GeneralSetting.ArgId(ArgDescriptions);
-const CommandGeneralArgId = GeneralSetting.Command.ArgId(ArgDescriptions);
+// const CommandGeneralArgId = GeneralSetting.Command.ArgId(ArgDescriptions);
 
-const GenerateSetting = @import("./commands/Generate.zig");
-const GenerateCommandArgId = GenerateSetting.ArgId(ArgDescriptions);
+// const GenerateSetting = @import("./commands/Generate.zig");
+// const GenerateCommandArgId = GenerateSetting.ArgId(ArgDescriptions);
 
-const InitializeSetting = @import("./commands/Initialize.zig");
-const InitializeCommandArgId = InitializeSetting.InitArgId(ArgDescriptions);
+// const InitializeSetting = @import("./commands/Initialize.zig");
+// const InitializeCommandArgId = InitializeSetting.InitArgId(ArgDescriptions);
 
-const SubcommandHelp = struct {
-    pub usingnamespace core.settings.ArgHelp(core.SubcommandArgId, ArgDescriptions);
-    pub const options: core.settings.ArgHelpOption = .{.category_name = "Subcommands"};
-};
+// const SubcommandHelp = struct {
+//     pub usingnamespace core.settings.ArgHelp(core.SubcommandArgId, ArgDescriptions);
+//     pub const options: core.settings.ArgHelpOption = .{.category_name = "Subcommands"};
+// };
 
-pub const ArgHelpSetting = struct {
-    pub const Tag = enum {
-        general,
-        subcommand,
-        cmd_general,
-        cmd_generate,
-        cmd_init_default,
-        cmd_init_config,
+pub const ArgHelp = struct {
+    pub const Config = struct {
+        tag: ArgHelp.Config.Tag,
+        members: []const ArgHelp.Config,
+
+        const Self = @This();
+
+        pub const Tag = enum {
+            toplevel,
+            args,
+            subcommands,
+            generate,
+            init_config,
+            init_default,
+        };
     };
 
-    tags: []const Tag,
-    command: ?core.SubcommandArgId,
+    pub const Render = struct {
+        policy: ArgHelp.Render.Policy,
+        config: *const ArgHelp.Config,
 
-    pub fn help(self: ArgHelpSetting, writer: anytype) !void {
-        try writer.print("usage: {s} [General options] {s} [Subcommand options]\n\n", .{
-            @import("build_options").exe_name, 
-            if (self.command) |c| @tagName(c) 
-            else SubcommandHelp.options.category_name.?
-        });
+        pub const Policy = enum {
+            command_root,
+            command_list,
+        };
+    };
 
-        for (self.tags) |tag| {
-            switch (tag) {
-                .general => {
-                    try core.settings.showHelp(writer, GeneralSettingArgId);
-                },
-                .cmd_general => {
-                    try core.settings.showHelp(writer, CommandGeneralArgId);
-                },
-                .subcommand => {
-                    try core.settings.showSubcommandAll(writer, core.SubcommandArgId, SubcommandHelp);
-                },
-                .cmd_generate => {
-                    try core.settings.showHelp(writer, GenerateCommandArgId);
-                },
-                .cmd_init_default, .cmd_init_config => {
-                    try core.settings.showHelp(writer, InitializeCommandArgId);
-                },
-            }
-        }
-    }
+
+
+
+
+//     pub fn help(self: ArgHelpSetting, writer: anytype) !void {
+//         try writer.print("usage: {s} [General options] {s} [Subcommand options]\n\n", .{
+//             @import("build_options").exe_name, 
+//             if (self.command) |c| @tagName(c) 
+//             else SubcommandHelp.options.category_name.?
+//         });
+
+//         for (self.tags) |tag| {
+//             switch (tag) {
+//                 .general => {
+//                     try core.settings.showHelp(writer, GeneralSettingArgId);
+//                 },
+//                 .cmd_general => {
+//                     try core.settings.showHelp(writer, CommandGeneralArgId);
+//                 },
+//                 .subcommand => {
+//                     try core.settings.showSubcommandAll(writer, core.SubcommandArgId, SubcommandHelp);
+//                 },
+//                 .cmd_generate => {
+//                     try core.settings.showHelp(writer, GenerateCommandArgId);
+//                 },
+//                 .cmd_init_default, .cmd_init_config => {
+//                     try core.settings.showHelp(writer, InitializeCommandArgId);
+//                 },
+//             }
+//         }
+//     }
+    pub const toplevel: ArgHelp.Config = .{ .tag = .toplevel, .members = &.{ subcommands } };
+    pub const args: ArgHelp.Config = .{ .tag = .args, .members = &.{} };
+    pub const subcommands: ArgHelp.Config = .{ .tag = .subcommands, .members = &.{generate, init_config, } };
+    pub const generate: ArgHelp.Config = .{ .tag = .generate, .members = &.{args} };
+    pub const init_config: ArgHelp.Config = .{ .tag = .init_config, .members = &.{args} };
 };
 
-pub const GeneralHelpSetting: ArgHelpSetting = .{ .tags = &.{.subcommand, .general}, .command = null };
